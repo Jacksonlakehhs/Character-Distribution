ae33186f02e0a4f748f96f368326172fdc14fe03
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2018-10-01 13:06:09 -0400

    Updated from Brython Server: 10/1/2018 1:06:08 PM

--- distribution.py
+++ distribution.py
@@ -1,7 +1,7 @@
 """
 distribution.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Jackson Lake
+Credit: Mr. Dennison
 
 Assignment:
 
@@ -36,3 +36,10 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+string = input("Please enter a string of text (the bigger the better): ")
+print('The distribution of characters in "{0}" is: '.format(string))
+lttrs = list(string)
+print(lttrs.count('a'))
+
+
+
